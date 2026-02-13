32d81ed47fc00d625d5406f24d2d96fe936bcadc
PorJaruwan <Jaruwan.Kaisiriphok@email.com>
2026-02-13 20:43:47 +0700

    update search update url main page

--- app/main.py
+++ app/main.py
@@ -25,7 +25,8 @@ app = FastAPI()
 origins = [
     "https://we-l-l-plus-admin-35c1o0.flutterflow.app",
     "https://preview.flutterflow.io",  
-    "https://well-plus-trial.flutterflow.app/",  
+    "https://well-plus-trial.flutterflow.app", 
+    
     # Add custom domains if any:
     # "https://yourdomain.com",
 ]
